d330b687d80727f200651c940a862139f91e48cb
Yusuf B <drukveyul@gmail.com>
2024-04-14 11:04:08 +0100

    Add `test_fetch_current_events.py` to workflow

--- .github/scripts/fetch_current_events.py
+++ .github/scripts/fetch_current_events.py
@@ -24,47 +24,52 @@ def remove_pictured(r):
           pos -= 1
       if start >= 0 and end >= 0:
           r = r[0:max(start, 0)] + r[1+end:]
+  r = r.replace(' <i></i>', '').replace('<i></i>', '').replace('  ', ' ').replace(' .', '.').replace(' ,', ',')
   return r
-r = requests.get('https://en.wikipedia.org/wiki/Portal:Current_events').text
-r = r.split('id="Topics_in_the_news"', 1)[1].split('<ul>', 1)[1].split('</ul>', 1)[0]
-r = ' '.join(r.splitlines())
-ab = bleach.clean(r, attributes = {'a': ['href', 'title']}, strip=True)
 
-if ab != r:
-  print('content change after bleach!')
-  print('before:')
-  print(r)
-  print('after:')
-  print(ab)
+def process_content():
+  r = requests.get('https://en.wikipedia.org/wiki/Portal:Current_events').text
+  r = r.split('id="Topics_in_the_news"', 1)[1].split('<ul>', 1)[1].split('</ul>', 1)[0]
+  r = ' '.join(r.splitlines())
+  ab = bleach.clean(r, attributes = {'a': ['href', 'title']}, strip=True)
 
-r = '<ul>' + ab + '</ul>'
-r = r.replace("'", r"\'")
-ar = r 
-ar = remove_pictured(ar)
-ar = ar.replace(' <i></i>', '').replace('<i></i>', '').replace('  ', ' ').replace(' .', '.').replace(' ,', ',')
+  if ab != r:
+    print('content change after bleach!')
+    print('before:')
+    print(r)
+    print('after:')
+    print(ab)
 
-if ar != r:
-  print('content change after removing picture text:')
-  print('before:')
-  print(r)
-  print('after:')
-  print(ar)          
+  r = '<ul>' + ab + '</ul>'
+  r = r.replace("'", r"\'")
+  ar = r 
+  ar = remove_pictured(ar)
+  
+  if ar != r:
+    print('content change after removing picture text:')
+    print('before:')
+    print(r)
+    print('after:')
+    print(ar)          
 
-r = "const content = '" + ar + "';"
+  r = "const content = '" + ar + "';"
 
-env_file = os.getenv('GITHUB_ENV')
+  env_file = os.getenv('GITHUB_ENV')
 
-with open('content.js') as f: 
-  s = f.read().strip()
-  print('current file content:')
-  print(s)
-  print('fetched content:')
-  print(r)
-  if s == r:
-    print('no content change')
-    with open(env_file, 'a') as f:
-      f.write("CHANGE=false\n")
-  else:
-    print('content change')
-    with open(env_file, 'a') as f:
-      f.write("CHANGE=true\nCONTENT=" + r)
+  with open('content.js') as f: 
+    s = f.read().strip()
+    print('current file content:')
+    print(s)
+    print('fetched content:')
+    print(r)
+    if s == r:
+      print('no content change')
+      with open(env_file, 'a') as f:
+        f.write("CHANGE=false\n")
+    else:
+      print('content change')
+      with open(env_file, 'a') as f:
+        f.write("CHANGE=true\nCONTENT=" + r)
+
+if __name__ == '__main__':
+  process_content()
